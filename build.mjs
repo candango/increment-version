032bdd3e8eab3a7394dad1059b15dacfd243f57f
Flavio Garcia <piraz@candango.org>
2025-05-03 00:20:03 -0400

    feat(action): add bundle generated by esbuild
    Refs: #2

---
+++ build.mjs
@@ -0,0 +1,25 @@
+import * as esbuild from "esbuild";
+import {entryPoints} from "./entryPoints.mjs";
+
+// The fix was posted here:
+// https://medium.com/geekculture/build-a-library-with-esbuild-23235712f3c
+await esbuild.build({
+    entryPoints: entryPoints,
+    bundle: true,
+    minify: true,
+    platform: "node",
+    // splitting: true,
+    write: true,
+    treeShaking: true,
+    sourcemap: false,
+    format: "esm",
+    define: { gobal: "window" },
+    target: ["esnext"],
+    outdir: "dist",
+    logLevel: "info",
+    legalComments: "none",
+    // outExtension: { '.js': '.cjs' },
+    // target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+    plugins:[
+    ]
+}).catch(() => process.exit(1));
---
+++ entryPoints.mjs
@@ -0,0 +1,3 @@
+export const entryPoints =  [
+    { out: "index", in: "index.js" },
+];
