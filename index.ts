3bee2ad9d99508f0097dcc0dab957cc96d9470d0
Flavio Garcia <piraz@candango.org>
2025-05-13 00:10:33 -0400

    chore(action): use app token to generate requests
    Refs: #4

--- index.ts
+++ index.ts
@@ -16,32 +16,20 @@ async function run(): Promise<void> {
         });
 
         const installation = await octokit.apps.getAuthenticated();
+        const installationId: number = (installation.data?.id) as number;
+        const { data: installationToken } = await octokit.apps.createInstallationAccessToken({
+            installation_id: installationId
+        });
+        const appToken: string = installationToken.token;
+        const tokenOctokit = new Octokit({ auth: appToken });
 
-        // If we need to use the token directly, we can do using this method
-        // const appToken: string = installationToken.token;
-        // const tokenOctokit = new Octokit({ auth: appToken });
-        // const { data: repoVar } = await tokenOctokit.request('GET /repos/{owner}/{repo}/actions/variables/{name}', {
-        //     owner: process.env.GITHUB_REPOSITORY!.split('/')[0],
-        //     repo: process.env.GITHUB_REPOSITORY!.split('/')[1],
-        //     name: currentVersionVar
-        // });
         const owner: string =  process.env.GITHUB_REPOSITORY!.split("/")[0];
         const repo: string =  process.env.GITHUB_REPOSITORY!.split("/")[1];
-        const { data: installationToken } =  await octokit.apps.getRepoInstallation({
-            owner,
-            repo
-        });
-        const installationId: number = (installation.data?.id) as number;
-        const installationOctokit = new Octokit({
-            authStrategy: createAppAuth,
-            auth: { appId, privateKey, installationId }
-        });
-
 
         core.setOutput("owner", owner);
         core.setOutput("repo", repo);
         const currentVersionVar: string = core.getInput("current-version-variable");
-        const { data: repoVar } = await installationOctokit.request("GET /repos/{owner}/{repo}/actions/variables/{name}", {
+        const { data: repoVar } = await tokenOctokit.request("GET /repos/{owner}/{repo}/actions/variables/{name}", {
             owner: owner,
             repo: repo,
             name: currentVersionVar
@@ -52,7 +40,7 @@ async function run(): Promise<void> {
         patch += 1;
         const newVersion: string = `${major}.${minor}.${patch}`;
 
-        await installationOctokit.request("PATCH /repos/{owner}/{repo}/actions/variables/{name}", {
+        await tokenOctokit.request("PATCH /repos/{owner}/{repo}/actions/variables/{name}", {
             owner: owner,
             repo: repo,
             name: currentVersionVar,
