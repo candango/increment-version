57079efab2f8e7aebc14b5cc0e84102e879e19b6
Flavio Garcia <piraz@candango.org>
2025-05-12 21:36:51 -0400

    refactor(action): point the action to run as a JavaScript
    Refs: #4

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 try {
-    const nameToGreet = core.getInput("who-to-greet");
-    console.log(`Hello, ${nameToGreet}`);
+    const currentVersion = core.getInput("current-version");
+    console.log(`The current version is:  ${currentVersion}`);
 
     const paylod = github.context.payload;
     console.log(`Event dispached by: ${paylod.sender.login}`);
