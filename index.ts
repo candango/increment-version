ca778c3e9323ab1f28838473cb339a6aeab08b33
Flavio Garcia <piraz@candango.org>
2025-05-13 21:34:03 -0400

    chore(action): test auth app directly
    Refs: #4

--- index.ts
+++ index.ts
@@ -10,12 +10,22 @@ async function run(): Promise<void> {
         
         const appId: string = core.getInput("app-id", {required: true});
         const privateKey: string = core.getInput("private-key", {required: true});
-        const octokit = new Octokit({
-            authStrategy: createAppAuth,
-            auth: {appId, privateKey}
+        
+        // const octokit = new Octokit({
+        //     authStrategy: createAppAuth,
+        //     auth: {appId, privateKey}
+        // });
+
+        const auth = createAppAuth({
+            appId: appId,
+            privateKey: privateKey
         });
 
-        await octokit.apps.getAuthenticated();
+        const appAuthentication = await auth({ type: "app" });
+
+        core.setOutput("authentication", appAuthentication);
+        // await octokit.apps.getAuthenticated();
+
         // const installationId: number = (installation.data?.id) as number;
         // const { data: installationToken } = await octokit.apps.createInstallationAccessToken({
         //     installation_id: installationId
@@ -23,31 +33,31 @@ async function run(): Promise<void> {
         // const appToken: string = installationToken.token;
         // const tokenOctokit = new Octokit({ auth: appToken });
 
-        const owner: string =  process.env.GITHUB_REPOSITORY!.split("/")[0];
-        const repo: string =  process.env.GITHUB_REPOSITORY!.split("/")[1];
-
-        core.setOutput("owner", owner);
-        core.setOutput("repo", repo);
-        const currentVersionVar: string = core.getInput("current-version-variable");
-        const { data: repoVar } = await octokit.request("GET /repos/{owner}/{repo}/actions/variables/{name}", {
-            owner: owner,
-            repo: repo,
-            name: currentVersionVar
-        });
-        
-        let currentVersion: string = repoVar.value;
-        let [major, minor, patch]: number[] = currentVersion.split(".").map(Number);
-        patch += 1;
-        const newVersion: string = `${major}.${minor}.${patch}`;
-
-        await octokit.request("PATCH /repos/{owner}/{repo}/actions/variables/{name}", {
-            owner: owner,
-            repo: repo,
-            name: currentVersionVar,
-            value: newVersion
-        });
-
-        core.setOutput("new-version", newVersion);
+        // const owner: string =  process.env.GITHUB_REPOSITORY!.split("/")[0];
+        // const repo: string =  process.env.GITHUB_REPOSITORY!.split("/")[1];
+        //
+        // core.setOutput("owner", owner);
+        // core.setOutput("repo", repo);
+        // const currentVersionVar: string = core.getInput("current-version-variable");
+        // const { data: repoVar } = await octokit.request("GET /repos/{owner}/{repo}/actions/variables/{name}", {
+        //     owner: owner,
+        //     repo: repo,
+        //     name: currentVersionVar
+        // });
+        // 
+        // let currentVersion: string = repoVar.value;
+        // let [major, minor, patch]: number[] = currentVersion.split(".").map(Number);
+        // patch += 1;
+        // const newVersion: string = `${major}.${minor}.${patch}`;
+        //
+        // await octokit.request("PATCH /repos/{owner}/{repo}/actions/variables/{name}", {
+        //     owner: owner,
+        //     repo: repo,
+        //     name: currentVersionVar,
+        //     value: newVersion
+        // });
+        //
+        // core.setOutput("new-version", newVersion);
     } catch (error: any) {
         core.setFailed(`Action failed: ${error.message}`);
     }
