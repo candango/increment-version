c2a72bfc497476a30de01e4b9acf34741a5d962e
Flavio Garcia <piraz@candango.org>
2025-05-13 21:50:56 -0400

    chore(action): run octokit with the token generated
    Refs: #4

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import * as core from "@actions/core";
 import { exec } from "@actions/exec";
 import { Octokit } from "@octokit/rest";
 import { createAppAuth } from "@octokit/auth-app";
+// import { resolve } from "path";
 
 async function run(): Promise<void> {
     try {
@@ -10,11 +11,6 @@ async function run(): Promise<void> {
         
         const appId: string = core.getInput("app-id", {required: true});
         const privateKey: string = core.getInput("private-key", {required: true});
-        
-        // const octokit = new Octokit({
-        //     authStrategy: createAppAuth,
-        //     auth: {appId, privateKey}
-        // });
 
         const auth = createAppAuth({
             appId: appId,
@@ -23,42 +19,41 @@ async function run(): Promise<void> {
 
         const appAuthentication = await auth({ type: "app" });
 
-        core.info(appAuthentication.expiresAt);
-        core.info(appAuthentication.token);
-        // await octokit.apps.getAuthenticated();
+        const octokit = new Octokit({
+            auth: appAuthentication.token
+        });
 
-        // const installationId: number = (installation.data?.id) as number;
-        // const { data: installationToken } = await octokit.apps.createInstallationAccessToken({
-        //     installation_id: installationId
-        // });
-        // const appToken: string = installationToken.token;
-        // const tokenOctokit = new Octokit({ auth: appToken });
+        const owner: string =  process.env.GITHUB_REPOSITORY!.split("/")[0];
+        const repo: string =  process.env.GITHUB_REPOSITORY!.split("/")[1];
 
-        // const owner: string =  process.env.GITHUB_REPOSITORY!.split("/")[0];
-        // const repo: string =  process.env.GITHUB_REPOSITORY!.split("/")[1];
-        //
-        // core.setOutput("owner", owner);
-        // core.setOutput("repo", repo);
-        // const currentVersionVar: string = core.getInput("current-version-variable");
-        // const { data: repoVar } = await octokit.request("GET /repos/{owner}/{repo}/actions/variables/{name}", {
-        //     owner: owner,
-        //     repo: repo,
-        //     name: currentVersionVar
-        // });
-        // 
-        // let currentVersion: string = repoVar.value;
-        // let [major, minor, patch]: number[] = currentVersion.split(".").map(Number);
-        // patch += 1;
-        // const newVersion: string = `${major}.${minor}.${patch}`;
-        //
-        // await octokit.request("PATCH /repos/{owner}/{repo}/actions/variables/{name}", {
-        //     owner: owner,
-        //     repo: repo,
-        //     name: currentVersionVar,
-        //     value: newVersion
-        // });
-        //
-        // core.setOutput("new-version", newVersion);
+        core.setOutput("owner", owner);
+        core.setOutput("repo", repo);
+        const currentVersionVar: string = core.getInput("current-version-variable");
+        const { data: repoVar } = await octokit.request("GET /repos/{owner}/{repo}/actions/variables/{name}", {
+            owner: owner,
+            repo: repo,
+            name: currentVersionVar
+        });
+
+        let currentVersion: string = repoVar.value;
+        let [major, minor, patch]: number[] = currentVersion.split(".").map(Number);
+        patch += 1;
+        const newVersion: string = `${major}.${minor}.${patch}`;
+
+        await octokit.request("PATCH /repos/{owner}/{repo}/actions/variables/{name}", {
+            owner: owner,
+            repo: repo,
+            name: currentVersionVar,
+            value: newVersion
+        });
+
+        await octokit.request("DELETE /installation/token", {
+            headers: {
+                auth: appAuthentication.token
+            }
+        });
+
+        core.setOutput("new-version", newVersion);
     } catch (error: any) {
         core.setFailed(`Action failed: ${error.message}`);
     }
